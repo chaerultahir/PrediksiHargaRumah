5c4c256029f762f2e702adbc71ebfe05f0a00ecd
Muh. Chaerul <75104748+chaerultahir@users.noreply.github.com>
2024-07-01 13:56:50 +0700

    Update streamlit_app(new).py

--- streamlit_app(new).py
+++ streamlit_app(new).py
@@ -23,7 +23,7 @@ test = st.sidebar.radio("Menu", ["Beranda", "Data", "Labelling", "Prediksi", "Ko
 
 # Halaman Beranda
 if test == "Beranda":
-    st.header("Halo Selamat Datang Semuanya :wave:")
+    st.header("Halo Selamat Datang :wave:")
     st.markdown("#### Ini merupakan website yang dapat memprediksi harga rumah di Kota Semarang sesuai dengan kriteria yang diinginkan oleh calon pembeli.")
     st.image('Peta Kota Semarang.png')
     st.markdown("#### Dengan adanya website ini, diharapkan dapat membantu para calon pembeli dalam menentukan harga rumah yang sesuai dan memenuhi kriteria rumah impiannya.")
